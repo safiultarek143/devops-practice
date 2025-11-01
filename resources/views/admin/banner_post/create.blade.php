c8af753b885c3edad6c698b731f25bdaf38d0d68
Safiul Tarek <safiultarek143@gmail.com>
2025-11-01 19:31:40 +0600

    commit 4

--- resources/views/admin/banner_post/create.blade.php
+++ resources/views/admin/banner_post/create.blade.php
@@ -42,7 +42,7 @@
                             <div class="form-group">
                                 <label for="description">Status1</label>
                                 <select name="status" id="" class="form-control">
-                                    <option value="">Choose status1</option>
+                                    <option value="">Choose status133</option>
                                             <option selected value="1">Published</option>
                                             <option value="0">Unpublished</option>
                                 </select>
